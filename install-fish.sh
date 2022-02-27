03d2b8460c09a320e6f8edc94df7b63b46a99871
minetaro12 <osawa2003@gmail.com>
2022-02-27 21:25:52 +0900

    update

---
+++ install-fish.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#install fisher
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+#install theme
+fisher install oh-my-fish/theme-bobthefish
--- install-vimrc.sh
+++ install-vimrc.sh
@@ -4,12 +4,10 @@ mkdir -p ~/.vim/colors
 mkdir -p ~/.vim/autoload
 
 # install vim-plug
-
 curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -o ~/.vim/autoload/plug.vim
 
 # install molokai
 curl https://raw.githubusercontent.com/minetaro12/dotfiles/master/vim/colors/molokai.vim -o ~/.vim/colors/molokai.vim
 
 # install vimrc
-
 curl https://raw.githubusercontent.com/minetaro12/dotfiles/master/vimrc -o ~/.vimrc
