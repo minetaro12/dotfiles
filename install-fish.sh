ba7ab434860e1e10e23e90d920ef9bb918e3611e
Oさん <77948036+minetaro12@users.noreply.github.com>
2022-07-31 11:24:29 +0900

    Update install-fish.sh

--- install-fish.sh
+++ install-fish.sh
@@ -5,3 +5,7 @@ curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
 #install theme
 fisher install oh-my-fish/theme-bobthefish
+
+#theme settings
+echo "set -g theme_display_user yes" >> ~/.config/fish/config.fish
+echo "set -g theme_display_hostname yes" >> ~/.config/fish/config.fish
