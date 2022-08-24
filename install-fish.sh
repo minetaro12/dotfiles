dd7fd0a5d55011a8f8cf860b361be5a42ffb2e44
minetaro12 <osawa2003@gmail.com>
2022-08-24 11:05:12 +0900

    update

--- install-fish.sh
+++ install-fish.sh
@@ -9,3 +9,7 @@ fisher install oh-my-fish/theme-bobthefish
 #theme settings
 echo "set -g theme_display_user yes" >> ~/.config/fish/config.fish
 echo "set -g theme_display_hostname yes" >> ~/.config/fish/config.fish
+
+#alias settings
+echo 'alias node="docker run -it --rm --user $(id -u):$(id -g) --volume $(pwd):$(pwd) --workdir $(pwd) --net=host node:18-alpine node"' >> ~/.config/fish/config.fish
+echo 'alias yarn="docker run -it --rm --user $(id -u):$(id -g) --volume $(pwd):$(pwd) --workdir $(pwd) --net=host node:18-alpine yarn"' >> ~/.config/fish/config.fish
