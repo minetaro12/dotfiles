80ce9c2799aaa2ce1f523ae8e246499f34cf8e0a
minetaro12 <osawa2003@gmail.com>
2022-10-04 19:07:48 +0900

    Add ln.sh

---
+++ ln.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+srcdir=$(dirname $0)
+
+# .vimrc
+if [ -e ${HOME}/.vimrc ]; then
+  echo "${HOME}/.vimrc already exists"
+else
+  ln -s ${srcdir}/vimrc ${HOME}/.vimrc
+fi
+
+# .vim
+if [ -d ${HOME}/.vim ]; then
+  echo "${HOME}/.vim already exists"
+else
+  ln -s ${srcdir}/vim ${HOME}/.vim
+  curl -fLo ${HOME}/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# .tmux.conf
+if [ -e ${HOME}/.tmux.conf ]; then
+  echo "${HOME}/.tmux.conf already exists"
+else
+  ln -s ${srcdir}/tmux.conf ${HOME}/.tmux.conf
+fi
