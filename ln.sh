9fc9aff36afbaead706269cea9475af654ee2326
minetaro12 <osawa2003@gmail.com>
2022-10-04 19:46:19 +0900

    update

--- ln.sh
+++ ln.sh
@@ -10,12 +10,12 @@ else
 fi
 
 # .vim
-#if [ -d ${HOME}/.vim ]; then
-#  echo "${HOME}/.vim already exists"
-#else
-#  ln -s ${srcdir}/vim ${HOME}/.vim
-#  curl -fLo ${HOME}/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-#fi
+if [ -d ${HOME}/.vim ]; then
+  echo "${HOME}/.vim already exists"
+else
+  #ln -s ${srcdir}/vim ${HOME}/.vim
+  curl -fLo ${HOME}/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
 # .tmux.conf
 if [ -e ${HOME}/.tmux.conf ]; then
