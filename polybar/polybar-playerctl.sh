76b7be645bd2df565343b215d562c163df2572f5
minetaro12 <osawa2003@gmail.com>
2022-11-02 21:39:46 +0900

    update

--- polybar/polybar-playerctl.sh
+++ polybar/polybar-playerctl.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 
-status=$(playerctl status)
+status=$(playerctl status 2> /dev/null)
 
 if [ "$status" = "Playing" -o "$status" = "Paused" ]; then
   title=$(playerctl metadata xesam:title)
   artist=$(playerctl metadata xesam:artist)
 
+  #アーティスト名が30文字以上の場合は省略
   if [ "${#artist}" -ge 30 ]; then
     showa="${artist:0:30}..."
   else
     showa=$artist
   fi
 
+  #タイトル名が30文字以上の場合は省略
   if [ "${#title}" -ge 30 ]; then
     showt="${title:0:30}..."
   else
     showt=$title
   fi
 
-  if [ "$status" == "Playing" ]; then
+  #再生中と一時停止中の判定
+  if [ "$status" = "Playing" ]; then
     shows=""
   else
     shows=""
   fi
 
-  echo "$shows $showa - $showt"
+  #アーティスト名がない場合は表示しない
+  if [ "$showa" = "" ]; then
+    echo "$shows $showt"
+  else
+    echo "$shows $showa - $showt"
+  fi
+
+else
+  echo
 fi
