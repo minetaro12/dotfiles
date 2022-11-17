2ea837be44c5e4685b9273ef760ba89a951d878d
minetaro12 <osawa2003@gmail.com>
2022-11-17 09:41:32 +0900

    update

--- wezterm.lua
+++ wezterm.lua
@@ -1,8 +1,11 @@
 local wezterm = require 'wezterm'
+
 return {
+  default_prog = { '/usr/bin/fish', '-l' },
   use_ime = true,
+  --ime_preedit_rendering = 'System',
   tab_bar_at_bottom = true,
-  color_scheme = "Bright (base16)",
+  color_scheme = "Brogrammer",
   font = wezterm.font_with_fallback {
     'Hack Nerd Font',
     'Noto Sans CJK JP',
