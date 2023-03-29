b7115f1dc3d479d17a911e8a78eed20c0db0f92c
Oさん <77948036+minetaro12@users.noreply.github.com>
2023-03-29 16:41:09 +0900

    update

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,6 @@ del alias:curl
 # キーバインドの設定
 Set-PSReadlineKeyHandler -Key ctrl+d -Function DeleteCharOrExit
 Set-PSReadLineKeyHandler -Key tab -Function MenuComplete
+
+# oh-my-posh
+oh-my-posh init powershell | iex
