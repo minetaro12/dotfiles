111c85258bcd709eb41dffcf197c69de281ac7bd
minetaro12 <osawa2003@gmail.com>
2023-03-29 23:20:24 +0900

    update

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -6,4 +6,4 @@ Set-PSReadlineKeyHandler -Key ctrl+d -Function DeleteCharOrExit
 Set-PSReadLineKeyHandler -Key tab -Function MenuComplete
 
 # oh-my-posh
-oh-my-posh init powershell | iex
+oh-my-posh init powershell -c $env:POSH_THEMES_PATH\blueish.omp.json | iex
