572f967f1438e5ed7d5940b56835f2ec657ce324
minetaro12 <osawa2003@gmail.com>
2023-12-02 17:18:07 +0900

    update

--- scripts/trackpoint-accel.sh
+++ scripts/trackpoint-accel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xinput list | grep pointer | grep Lenovo | grep -e "id=[0-9]*" -o | cut -d "=" -f 2 | xargs -t -n1 bash -c 'xinput set-prop $0 "libinput Accel Speed" 0.7' 2> /dev/null &
+xinput list | grep pointer | grep Lenovo | grep -e "id=[0-9]*" -o | cut -d "=" -f 2 | xargs -t -n1 bash -c 'xinput set-prop $0 "libinput Accel Speed" 0.9' 2> /dev/null &
